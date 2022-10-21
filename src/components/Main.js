38ed548e4574437881b5c44208af330d818e26d5
mattlockyer <mattdlockyer@gmail.com>
2022-10-21 07:21:47 -0700

    update lib path

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,7 +13,7 @@ import {
 	isSignedIn,
 	signAndSendTransactions,
 	MIN_NEW_ACCOUNT_ASK,
-} from '../../neth/lib';
+} from '../../neth';
 
 import { networkId } from '../App';
 import { Info } from 'react-feather';
